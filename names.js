6d37a13de8ea2c6141e27c846ebcd4b8d43af65d
David Ibia <david.ibia@boxmarshall.com>
2018-08-05 02:51:01 +0100

    feat: added content

---
+++ names.js
@@ -0,0 +1,22 @@
+  const names = [
+      'Mike',
+      'Paul',
+      'John',
+      'Hugo',
+      'Lisa',
+      'Alex',
+      'Sean',
+      'Carlos',
+      'Miguel',
+      'Smith',
+      'Hernandez',
+      'Murdock',
+      'Doug',
+      'Swisher',
+      'Garrison',
+      'Barber',
+      'Harrick',
+      'Abramson'
+  ]; // you can add more names
+
+  module.exports = names;
